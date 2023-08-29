85e50d6ea45278d72de736023e4c783609f85cc2
Marcelo Monge <111376835+MaSuuUx@users.noreply.github.com>
2023-08-28 21:27:29 -0600

    Primer commit

---
+++ Sismologia/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Sismologia
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicio));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BtnIngresar = new System.Windows.Forms.Button();
+            this.BtnPrint = new System.Windows.Forms.Button();
+            this.BtnCerrar = new System.Windows.Forms.Button();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(199, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(614, 88);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Departamento de Sismologia \nEscuela de Ingenieria Civil";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Sismologia.Properties.Resources.Escudo_de_la_Universidad_de_El_Salvador;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(181, 165);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(311, 50);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingrese la magnitud del \nsismo que desea registrar:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(348, 262);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 26);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // BtnIngresar
+            // 
+            this.BtnIngresar.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.BtnIngresar.Font = new System.Drawing.Font("Verdana", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnIngresar.ForeColor = System.Drawing.Color.White;
+            this.BtnIngresar.Location = new System.Drawing.Point(613, 250);
+            this.BtnIngresar.Name = "BtnIngresar";
+            this.BtnIngresar.Size = new System.Drawing.Size(132, 50);
+            this.BtnIngresar.TabIndex = 4;
+            this.BtnIngresar.Text = "Ingresar";
+            this.BtnIngresar.UseVisualStyleBackColor = false;
+            this.BtnIngresar.Click += new System.EventHandler(this.BtnIngresar_Click);
+            // 
+            // BtnPrint
+            // 
+            this.BtnPrint.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.BtnPrint.Font = new System.Drawing.Font("Verdana", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPrint.ForeColor = System.Drawing.Color.White;
+            this.BtnPrint.Location = new System.Drawing.Point(234, 357);
+            this.BtnPrint.Name = "BtnPrint";
+            this.BtnPrint.Size = new System.Drawing.Size(140, 54);
+            this.BtnPrint.TabIndex = 5;
+            this.BtnPrint.Text = "Imprimir datos ";
+            this.BtnPrint.UseVisualStyleBackColor = false;
+            this.BtnPrint.Click += new System.EventHandler(this.BtnPrint_Click);
+            // 
+            // BtnCerrar
+            // 
+            this.BtnCerrar.BackColor = System.Drawing.Color.Crimson;
+            this.BtnCerrar.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCerrar.ForeColor = System.Drawing.Color.White;
+            this.BtnCerrar.Location = new System.Drawing.Point(495, 357);
+            this.BtnCerrar.Name = "BtnCerrar";
+            this.BtnCerrar.Size = new System.Drawing.Size(143, 54);
+            this.BtnCerrar.TabIndex = 6;
+            this.BtnCerrar.Text = "Cerrar ";
+            this.BtnCerrar.UseVisualStyleBackColor = false;
+            this.BtnCerrar.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(848, 617);
+            this.Controls.Add(this.BtnCerrar);
+            this.Controls.Add(this.BtnPrint);
+            this.Controls.Add(this.BtnIngresar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Inicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sismos";
+            this.Load += new System.EventHandler(this.Inicio_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BtnIngresar;
+        private System.Windows.Forms.Button BtnPrint;
+        private System.Windows.Forms.Button BtnCerrar;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+    }
+}
+
---
+++ Sismologia/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ -------------------------------------------------------------------------------------------
+ ===> NOMBRE: HECTOR MARCELO MONGE CABALLERO
+ ===> CARNET: MC23084
+ ===> PROFESOR: ING. BLADIMIR DIAZ
+ ===> GL: 18
+
+ENUNCIADO: El Departamento de Sismología de la Escuela de Ingeniería Civil de la Facultad de
+Ingeniería y Arquitectura de la Universidad de El Salvador, necesita una solución que le
+permita registrar la magnitud de los sismos que acontecen diariamente. Es decir, cada vez
+que haya un sismo, se debe de registrar su magnitud. Al final del día se desea imprimir la
+cantidad de sismos acontecidos.
+
+ -------------------------------------------------------------------------------------------
+ */
+namespace Sismologia
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void Inicio_Load(object sender, EventArgs e)
+        {
+            BtnIngresar.Enabled = false;//DESHABILITAR BOTON AL CARAGAR EL FORMS
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();//CERRAR FORM
+        }
+
+        //EVENTO PARA EL BtnIngresar
+        private void BtnIngresar_Click(object sender, EventArgs e)
+        {
+            string entradaData = textBox1.Text;
+
+            if (EsNumeroDecimal(entradaData))
+            {
+                decimal numero = decimal.Parse(entradaData);
+
+                GuardarEnArchivo(numero.ToString()); // GUARDAR ARCHIVOS
+            }
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            controlBtn();
+        }
+
+        private void controlBtn()
+        {
+            string entaradaData = textBox1.Text;
+
+            if (!string.IsNullOrWhiteSpace(entaradaData) && EsNumeroDecimal(entaradaData))
+            {
+                BtnIngresar.Enabled = true; // HABILITA BOTON SI EL CAMPO ES VALIDO
+                errorProvider.Clear(); // LIMPIA MSJ ERROR
+            }
+            else
+            {
+                BtnIngresar.Enabled = false; // DESHABILITA BOTON SI EL CAMPO NO ES VALIDO
+                errorProvider.SetError(textBox1, "Debe ingresar numeros en la escala de Richter (decimales)"); //MSJ DE ERROR
+            }
+        }
+
+        static bool EsNumeroDecimal(string input)
+        {
+            string patron = @"^[+-]?\d+(\.\d+)?$";
+            return System.Text.RegularExpressions.Regex.IsMatch(input, patron);
+        }
+
+        private void GuardarEnArchivo(string contenido)
+        {
+            try
+            {
+                string archivo = "Datos.txt";
+                string fechaActual = DateTime.Now.ToString(); // FECHA ACTUAL
+
+                using (StreamWriter sw = File.AppendText(archivo))
+                {
+                    sw.WriteLine(fechaActual + " - " + contenido); // GUARDAR FECHA Y SU CONTENIDO
+                }
+
+                MessageBox.Show("Datos guardados con éxito");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar en el archivo: " + ex.Message);
+            }
+        }
+
+        //METODO PARA IMPRIMIR LOS ARCHIVOS
+        private void Imprimir()
+        {
+            try
+            {
+                string archivo = "Datos.txt";
+
+                if (File.Exists(archivo))//VERIFICA SI EL ARCHIVO EXISTE
+                {
+                    string[] lineas = File.ReadAllLines(archivo);//LEE CADA LINEA DEL ARCHIVO Y GUARDA SUS DATOS
+                    List<string> datosHoy = new List<string>(); //CREA UNA LISTA PARA GUARDAR LOS DATOS
+
+                    foreach (var linea in lineas) //RECORRER CADA LINEA DEL ARCHIVO
+                    {
+                        string[] partes = linea.Split(new[] { " - " }, StringSplitOptions.None);
+
+                        if (partes.Length == 2)
+                        {
+                            string fechaString = partes[0];//EXTRAER LA FECHA
+                            if (DateTime.TryParse(fechaString, out DateTime fecha))//CONVIERTE STRING A DATETIME
+                            {
+                                if (fecha.Date == DateTime.Now.Date) // FILTRA Y COMPARA CON LA FECHA DEL PC
+                                {
+                                    datosHoy.Add(linea);
+                                }
+                            }
+                        }
+                    }
+
+                    if (datosHoy.Count > 0)//VERIFICA SI HAY DATOS ENCONTRADOS
+                    {
+                        string datosParaImprimir = string.Join(Environment.NewLine, datosHoy);
+                        MessageBox.Show("Datos ingresados hoy:\n" + datosParaImprimir, "Datos en el archivo");
+                    }
+                    else
+                    {
+                        MessageBox.Show("No hay datos ingresados hoy.", "Datos en el archivo");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El archivo no existe.", "Datos en el archivo");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al imprimir los datos: " + ex.Message);
+            }
+
+        }
+
+        //EVENTO DEL BtnPrint_Cilck
+        private void BtnPrint_Click(object sender, EventArgs e)
+        {
+            Imprimir();
+        }
+    }
+}
+
+
+
